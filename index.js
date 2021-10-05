da49533284909984bfafb39109ad5a24724b3d89
Jeff-Adler <Jeffm.adler@gmail.com>
2021-10-05 19:29:01 -0400

    fixed typeDefs

--- index.js
+++ index.js
@@ -7,9 +7,52 @@ const schema = {
 };
 
 const typeDefs = `
-type Query {
-  hello(name: String): String!
-}
+  type User {
+    id          :ID!
+    name        :String!
+    polls       :[Poll]
+  }
+  type Poll {
+    id          :ID!
+    description :String!
+    user        :User!
+    options     :[Option!]
+    votes       :[Vote]
+  }
+  type Option {
+    id          :ID!
+    text        :String!
+    poll        :Poll!
+    votes       :[Vote]
+  }
+  type Vote {
+    id          :ID!
+    user        :User!
+    poll        :Poll!
+    option      :Option!
+  }
+  type Query {
+    users: [User]
+    polls: [Poll]
+    votes: [Vote]
+    user(id: ID!): User
+    poll(id: ID!): Poll
+  }
+  type Mutation {
+    createUser(
+      name: String!
+    ): User
+    createPoll(
+      description: String!
+      id: ID!
+      options: [String!]
+    ): Poll
+    createVote(
+      userID: ID!
+      pollID: ID!
+      optionID: ID!
+    ): Vote
+  }
 `;
 
 const resolvers = {
