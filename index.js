991796d0a4c1f051d160110574b7d20fc22e84c4
Jeff-Adler <Jeffm.adler@gmail.com>
2021-10-05 20:08:07 -0400

    implemented gql

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const typeDefs = `
+const typeDefs = gql`
   type User {
-      id          :ID!
-      name        :String!
-      polls       :[Poll]
-    }
+    id: ID!
+    name: String!
+    polls: [Poll]
+  }
 
-    type Poll {
-      id          :ID!
-      description :String!
-      user        :User!
-      options     :[Option!]
-      votes       :[Vote]
-    }
+  type Poll {
+    id: ID!
+    description: String!
+    user: User!
+    options: [Option!]
+    votes: [Vote]
+  }
 
-    type Option {
-      id          :ID!
-      text        :String!
-      poll        :Poll!
-      votes       :[Vote]
-    }
+  type Option {
+    id: ID!
+    text: String!
+    poll: Poll!
+    votes: [Vote]
+  }
 
-    type Vote {
-      id          :ID!
-      user        :User!
-      poll        :Poll!
-      option      :Option!
-    }
+  type Vote {
+    id: ID!
+    user: User!
+    poll: Poll!
+    option: Option!
+  }
 
   type Query {
     users: [User]
@@ -41,21 +41,11 @@ const typeDefs = `
   }
 
   type Mutation {
-    createUser(
-      name: String!
-      ): User
+    createUser(name: String!): User
 
-    createPoll(
-      description: String!
-      id: ID!
-      options: [String!]
-    ): Poll
+    createPoll(description: String!, id: ID!, options: [String!]): Poll
 
-    createVote(
-      userID: ID!
-      pollID: ID!
-      optionID: ID!
-    ): Vote
+    createVote(userID: ID!, pollID: ID!, optionID: ID!): Vote
   }
 `;
 
